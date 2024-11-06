2368b470b53a529c095032768d8fbad46f1c3e3f
Akash <akashkumbhar2912@gmail.com>
2024-11-06 09:27:23 +0530

    a

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,248 +1,179 @@
-/*
-#include<iostream>
-#define MAX 10
-using namespace std;
-
-typedef struct JOB{
-    int ID;
-    string name;
-}JOB;
-
-class Queue{
-    private:
-        JOB Q[MAX];
-        int front,rear;
-    public:
-        Queue(){
-        front = rear = -1;
-        }
-
-        //Check if queue is empty
-        bool isQueueEmpty(){
-            if (front == -1)
-                return true;
-            else
-                return false;
-        }
-
-        //Check if queue is full
-        bool isQueueFull(){
-            if(rear == MAX-1)
-                   return true;
-            else
-                return false;
-        }
-        void enQueue(JOB j){
-            if(! isQueueFull()){
-                if(front == -1){
-                    front=rear=0;
-                    Q[rear]=j;
-                }
-            else
-                Q[++rear]=j;
-            }
-            else
-             cout<<"\nQueue is full!!";
-        }
-
-        void dQueue(){
-
-            if(!isQueueEmpty()){
-                if(front == rear){
-                    cout<<"\n"<<Q[front].ID<<"  is deleted from Queue";
-						front=rear=-1;
-                }
-                else
-                {
-                    cout<<"\n"<<Q[front].ID<<"  is deleted from Queue";
-						front++;
-                }
-            }
-            else 
-                cout<<"\nQueue is Empty!!";
-        }
-
-        //Display Queue Contents
-        void displayQueue(){
-            int i;
-            if(!isQueueEmpty()){
-                cout<<"\nJob Queue is:\n";
-                
-                for(i=front;i<=rear;i++)
-                    cout<<"|"<<Q[i].ID<<"-"<<Q[i].name<<"| ";
-            }
-            else
-				cout<<"\n Queue Empty!!";
-        }
-	};
-   
-//Driver Code
-int main(){
-    int choice = 0;
-    JOB j;
-    Queue obj;
-    while(choice !=4){
-        cout<<"\n***************JOB QUEUE*****************";
-		cout<<"\n1. Add Job in Queue";
-		cout<<"\n2. Delete Job from Queue";
-		cout<<"\n3. Display Job Queue";
-		cout<<"\n4. Exit Application";
-		cout<<"\nWhat is your choice::";
-		cin>>choice;
-        switch(choice){
-            case 1:
-				cout<<"\nEnter Job ID:";
-				cin>>j.ID;
-				cout<<"\nEnter Job Name:";
-				cin>>j.name;
-				obj.enQueue(j);
-				break;
-			case 2:
-				obj.dQueue();
-				break;
-			case 3:
-				obj.displayQueue();
-				break;
-			case 4:
-				cout<<"\nGood By!!";
-				break;
-        }
-    }
-}
- */
-//Experiment 13
-#include <iostream>
-using namespace std;
-#define MAX 3
-
-class DQueue{
-    private:
-        int Q[MAX];
-        int front , rear;
-    public:
-        DQueue()
-        {
-            front=rear=-1;
-        }
-
-        //add element from front  in DQueue
-        void addBeginDQueue(int val){
-            if(front!=0){
-                if(front == -1){
-                    front=rear=0;
-                    Q[rear]=val;
-                }
-                else
-                    Q[--front]=val;
-            }
-            else
-                cout<<"\nDQueue is full at front!!";
-        }
-
-        //remove element from front at the DQueue
-        void delBeginDQueue(){
-            if(front != -1){
-
-            if(front == rear){
-                cout<< Q[front] <<"is deleted from DQueue";
-                front = rear = -1;
-            }
-            else{
-                cout<< Q[front] << "is deleted from DQueue";
-                front++;
-                } 
-            }
-            else
-            cout << "\nDQueue is empty!!";
-        }
-
-//Add Elemnt in DQueue at rear
-        void addEndDQueue(int val){
-            if(rear != MAX - 1){
-                if (front == -1){
-                    front = rear = 0;
-                    Q[rear] = val;
-                }
-                else
-                    Q[++rear] = val;
-            }
-                else
-                    cout << "\nDQueue is full at the rear!!";
-        }
-
-    //Remove Elemnt in DQueue at rear
-        void delEndDQueue(){
-                if (rear != -1){
-                if (front = rear){
-                    cout << "\n" <<Q[rear] << "is deleted from DQueue";
-                    front = rear= -1;
-                }
-                else{
-                    cout << Q[rear] << " is deleted from DQueue";
-                    rear--;
-                }
-            }
-            else
-                cout <<"\nDueue is Empty!!";       
-                 }
-        
-
-        //Display DQueue Contents
-         void displayDQueue(){
-                int i;
-                if (front != -1) {
-                    cout << "\nJOb DQueue is:\n";
-                    for(i = front;i<=rear;i++)
-                        cout << Q[i] << "";
-                }
-                else 
-                    cout <<"\nDueue is Empty!!";       
-                 
-        }
-};
-
-//Driver COde
-int main(){
-    int choice = 0, val;
-    DQueue obj;
-    while(choice != 6){
-        cout << "\n***************Double Ended Queue*****************";
-        cout << "\n1. Add element at Begin";
-        cout << "\n2. Delete element at Begin";
-        cout << "\n3. Add element at End";
-        cout << "\n4. Delete element at End";
-        cout << "\n5. Display DQueue";
-        cout << "\n6. Exit Application";
-        cout << "\nWhat is your choice::";
-        cin >> choice;
-
-        switch(choice){
-            case 1:
-                cout << "\nEnter Job ID:";
-                cin >> val;
-                obj.addBeginDQueue(val);
-                break;
-            case 2:
-                obj.delBeginDQueue();
-                break;
-            case 3:
-                cout << "\nEnter Job ID:";
-                cin >> val;
-                obj.addEndDQueue(val);
-                break;
-            case 4:
-                obj.delEndDQueue();
-                break;
-            case 5:
-                obj.displayDQueue();
-                break;
-            case 6:
-                cout << "\nGood Bye!!";
-                break;
-            default:
-                cout << "\nInvalid Choice! Please try again.";
-                break;
-        }
-    }
-    return 0;
-}
+Practical No:A1
+ Write C++ program to draw a concavepolygon and fill it with desired color using scan fill
+ algorithm. Apply the concept of inheritance
+ Code:
+ #include <conio.h>
+ #include <iostream>
+ #include <graphics.h>
+ #include <stdlib.h>
+ using namespacestd;
+ class point
+ {
+ public:
+ int x,y;
+ };
+ class poly
+ {
+ private:
+ point p[20];
+ int inter[20],x,y;
+ int v,xmin,ymin,xmax,ymax;
+ public:
+ int c;
+ void read();
+ void calcs();
+ void display();
+ void ints(float);
+ void sort(int);
+ };
+ void poly::read()
+ {
+ int i;
+ cout<<"\n Scan Fill Algorithm ";
+ cout<<"\n Enter Number Of Vertices Of Polygon: ";
+ cin>>v;
+ if(v>2)
+ {
+ for(i=0;i<v; i++) //ACCEPT THE VERTICES
+ {
+ cout<<"\nEnter co-ordinate no. "<<i+1<<" : ";
+ cout<<"\n\tx"<<(i+1)<<"=";
+ cin>>p[i].x;
+ cout<<"\n\ty"<<(i+1)<<"=";
+cin>>p[i].y;
+ }
+ p[i].x=p[0].x;
+ p[i].y=p[0].y;
+ xmin=xmax=p[0].x;
+ ymin=ymax=p[0].y;
+ }
+ else
+ cout<<"\n Enter valid no. of vertices.";
+ }
+ void poly::calcs()
+ {
+ for(int i=0;i<v;i++)
+ {
+ if(xmin>p[i].x)
+ xmin=p[i].x;
+ if(xmax<p[i].x)
+ xmax=p[i].x;
+ if(ymin>p[i].y)
+ ymin=p[i].y;
+ if(ymax<p[i].y)
+ ymax=p[i].y;
+ }
+ }
+ void poly::display()
+ {
+ int ch1;
+ char ch='y';
+ f
+ loat s,s2;
+ do
+ {
+ cout<<"\n\nMENU:";
+ cout<<"\n\n\t1 . Scan line Fill ";
+ cout<<"\n\n\t2 . Exit ";
+ cout<<"\n\nEnter your choice:";
+ cin>>ch1;
+ switch(ch1)
+ {
+ case 1:
+ s=ymin+0.01;
+ delay(100);
+ cleardevice();
+ while(s<=ymax)
+ {
+ ints(s);
+sort(s);
+ s++;
+ }
+ break;
+ case 2:
+ exit(0);
+ }
+ cout<<"Do you want to continue?: ";
+ cin>>ch;
+ }while(ch=='y' || ch=='Y');
+ }
+ void poly::ints(float z)
+ {
+ int x1,x2,y1,y2,temp;
+ c=0;
+ for(int i=0;i<v;i++)
+ {
+ x1=p[i].x;
+ y1=p[i].y;
+ x2=p[i+1].x;
+ y2=p[i+1].y;
+ if(y2<y1)
+ {
+ temp=x1;
+ x1=x2;
+ x2=temp;
+ temp=y1;
+ y1=y2;
+ y2=temp;
+ }
+ if(z<=y2&&z>=y1)
+ {
+ if((y1-y2)==0)
+ x=x1;
+ else
+ {
+ x=((x2-x1)*(z-y1))/(y2-y1);
+ x=x+x1;
+ }
+ if(x<=xmax && x>=xmin)
+ inter[c++]=x;
+ }
+ }
+ }
+ void poly::sort(int z) // sorting
+{
+ int temp,j,i;
+ for(i=0;i<v;i++)
+ {
+ line(p[i].x,p[i].y,p[i+1].x,p[i+1].y);
+ }
+ delay(100);
+ for(i=0; i<c;i+=2)
+ {
+ delay(100);
+ line(inter[i],z,inter[i+1],z);
+ }
+ }
+ int main() //main
+ {
+ int cl;
+ initwindow(500,600);
+ cleardevice();
+ poly x;
+ x.read();
+ x.calcs();
+ cleardevice();
+ cout<<"\n\tEnter The Color You Want :(In Range 0 To15 )->"; //selecting color
+ cin>>cl;
+ setcolor(cl);
+ x.display();
+ closegraph(); //closing graph
+ getch();
+ return 0;
+ }
+ /*
+ Input :
+ Numberof Vertices : 4
+ Cordinates 1st :
+ x1= 200
+ y1= 200
+ Cordinates 2st :
+ x2= 200
+ y2= 400
+Cordinates 3st :
+ x3= 400
+ y3= 200
+ Cordinates 4st :
+ x4= 400
+ y4= 400
+*/
